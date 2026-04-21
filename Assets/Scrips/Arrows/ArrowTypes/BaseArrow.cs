ab9a1bbcd24e289c3f97a2a621658d5185f285d8
JordiMartinezPl <jordimartinezplanas21@gmail.com>
2026-04-21 16:18:38 +0200

    basic health and enemy damage

--- Assets/Scrips/Arrows/ArrowTypes/BaseArrow.cs
+++ Assets/Scrips/Arrows/ArrowTypes/BaseArrow.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
-public class BaseArrow : Arrow {
+public class BaseArrow : Arrow 
+{
     public override ArrowType type => ArrowType.Base;
+    [SerializeField] private float baseDamage = 10f;
 
-    protected override void OnHit(Collider other,float damageMultiplier) {
-        Debug.Log("Impacto de flecha base" + $" con multiplicador de daño: {damageMultiplier}");
+    protected override void OnHit(Collider other, float damageMultiplier) 
+    {
+        IDamageable damageable = other.GetComponentInParent<IDamageable>();
+        if (damageable != null) 
+        {
+            damageable.TakeDamage(baseDamage * damageMultiplier);
+        }
     }
 }
--- Assets/Scrips/Arrows/ArrowTypes/BloodArrow.cs
+++ Assets/Scrips/Arrows/ArrowTypes/BloodArrow.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
-public class BloodArrow : Arrow {
+public class BloodArrow : Arrow 
+{
     public override ArrowType type => ArrowType.Blood;
+    [SerializeField] private float baseDamage = 15f;
 
-    protected override void OnHit(Collider other, float damageMultiplier) {
-        Debug.Log("Impacto de flecha de sangre" + $" con multiplicador de daño: {damageMultiplier}");
+    protected override void OnHit(Collider other, float damageMultiplier) 
+    {
+        IDamageable damageable = other.GetComponentInParent<IDamageable>();
+        if (damageable != null) 
+        {
+            damageable.TakeDamage(baseDamage * damageMultiplier);
+        }
     }
 }

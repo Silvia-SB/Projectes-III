1e5acf851e1b315ceb631719569beaec8a4b7163
Oriol Renom Plaja <oriol.renom.plaja@gmail.com>
2026-05-08 19:13:25 +0200

    SettingsMenu

--- Assets/Scrips/UI/SettingsMenuManager.cs
+++ Assets/Scrips/UI/SettingsMenuManager.cs
@@ -1,13 +1,44 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenuManager : MonoBehaviour
 {
-    public TMP_Dropdown graphisDropdown;
+    [Header("Graphics Settings")]
+    [SerializeField] private TMP_Dropdown graphicsDropdown;
+    
+    [Header("Audio Settings")]
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private AudioMixer audioMixer;
+    
+    [Header("Sensibility Settings")]
+    [SerializeField] private Slider sensitivitySlider;
+    public static event Action <float> OnSensibilityChanged;
     
     public void ChangeGraphicsQuality()
     {
-        Debug.Log(graphisDropdown.value);
-        QualitySettings.SetQualityLevel(graphisDropdown.value);
+        Debug.Log(graphicsDropdown.value);
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+    public void ChangeMasterVolume()
+    {
+        audioMixer.SetFloat("MasterVolume", masterSlider.value);
+    }
+    public void ChangeSfxVolume()
+    {
+        audioMixer.SetFloat("SFXVolume", sfxSlider.value);
+    }
+    public void ChangeMusicVolume()
+    {
+        audioMixer.SetFloat("MusicVolume", musicSlider.value);
+    }
+
+    public void ChangeSensibility()
+    {
+        OnSensibilityChanged?.Invoke(sensitivitySlider.value);
     }
 }

5455f420b0dd6a426af96da11e256f7ec2e299b2
Oriol Renom Plaja <oriol.renom.plaja@gmail.com>
2026-05-22 12:21:00 +0200

    Max active crows

--- Assets/Scrips/Enemy/Shoot/ShootCrow.cs
+++ Assets/Scrips/Enemy/Shoot/ShootCrow.cs
@@ -4,11 +4,17 @@ public class ShootCrow : MonoBehaviour
 {
     [SerializeField] private Transform spawnPointLeft;
     [SerializeField] private Transform spawnPointRight;
+    [SerializeField] private int maxActiveCrows = 5;
+    [SerializeField] private float recoverTime = 3f;
     
+    private float timer = 0f;
+    private int activeCrows = 0;
     private bool shootFromLeft = true; 
 
     public void ShootingCrow()
     {
+        if (activeCrows >= maxActiveCrows) return;
+        activeCrows++;
         Transform currentSpawnPoint = shootFromLeft ? spawnPointLeft : spawnPointRight;
         
         shootFromLeft = !shootFromLeft;
@@ -21,6 +27,19 @@ public class ShootCrow : MonoBehaviour
             cuervo.transform.rotation = currentSpawnPoint.rotation;
             
             cuervo.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (activeCrows <= 0) return;
+
+        timer += Time.deltaTime;
+
+        if (timer >= recoverTime)
+        {
+            activeCrows--;
+            timer = 0f;
         }
     }
 }

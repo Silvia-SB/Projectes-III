bf4922011d798ce985d299bb7d2e2329f4e6d8df
Oriol Renom Plaja <oriol.renom.plaja@gmail.com>
2026-05-28 21:14:10 +0200

    Testing

--- Assets/Scrips/Health/EnemyHealth.cs
+++ Assets/Scrips/Health/EnemyHealth.cs
@@ -1,17 +1,4 @@
 using UnityEngine;
 
 public class EnemyHealth : Health
-{
-    [Header("Enemy Rewards")]
-    [SerializeField] private int soulsToDrop = 15;
-
-    protected override void Die()
-    {
-        if (SoulManager.Instance != null)
-        {
-            SoulManager.Instance.AddSouls(soulsToDrop);
-        }
-
-        base.Die();
-    }
-}
+{}

c56984d0f6eec193a7a4f2533a73da2bf349e558
JordiMartinezPl <jordimartinezplanas21@gmail.com>
2026-05-28 17:24:23 +0200

    improve

---
+++ Assets/Scrips/Enviroment/DeactivateOnHits.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Linq;
+
+public class DeactivateOnHits : MonoBehaviour, IDamageable
+{
+    [Header("Settings")]
+    [SerializeField] private float maxHealth = 100f;
+    
+    [SerializeField] private DamageType[] validDamageTypes;
+
+    private float currentHealth;
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount, DamageType incomingDamageType)
+    {
+        ProcessDamage(amount, incomingDamageType);
+    }
+
+    public void TakeRecurrentDamage(float amount, float interval, int ticks, DamageType incomingDamageType)
+    {
+        ProcessDamage(amount, incomingDamageType);
+    }
+
+    private void ProcessDamage(float amount, DamageType damageType)
+    {
+        if (validDamageTypes != null && validDamageTypes.Length > 0 && !validDamageTypes.Contains(damageType))
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+        
+        if (currentHealth <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

00f22617e46bf38dbfda9f6bc0d1d75b452f52a8
jmillann <166120465+jmillann@users.noreply.github.com>
2026-05-29 19:52:17 +0200

    Dissolving enemy push

--- Assets/Scrips/Enemy/Enemy_Death_Dissolve/DissolvingController.cs
+++ Assets/Scrips/Enemy/Enemy_Death_Dissolve/DissolvingController.cs
@@ -1,48 +1,54 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DissolvingController : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private Renderer targetRenderer;
 
     [SerializeField] private float dissolveRate = 0.0125f;
-    [SerializeField] private float refreshRate = 0.025f;
+    private float timeToStartDissolve = 5f;
+    private bool isDissolving = false;
+    float counter = 0f;
+
 
     private Material[] materials;
 
     private void Start()
     {
-        if (meshRenderer != null)
+        if (targetRenderer != null)
         {
-            materials = meshRenderer.materials;
+            materials = targetRenderer.materials;
         }
+        timeToStartDissolve = Time.time + timeToStartDissolve;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Time.time>= timeToStartDissolve&& !isDissolving)
+        { 
+            isDissolving = true;
+        }
+
+        if (isDissolving&&counter<1f)
         {
-            StartCoroutine(DissolveCo());
+            DissolveCo();
+            
         }
     }
 
-    private IEnumerator DissolveCo()
+    private void DissolveCo()
     {
         if (materials == null || materials.Length == 0)
-            yield break;
-
-        float counter = 0;
-
-        while (materials[0].GetFloat("_DissolveAmount") < 1)
         {
+            Debug.LogWarning("No materials found on the target renderer.");
+        }
             counter += dissolveRate;
-
             for (int i = 0; i < materials.Length; i++)
             {
                 materials[i].SetFloat("_DissolveAmount", counter);
             }
-
-            yield return new WaitForSeconds(refreshRate);
-        }
-    }
+        
+    }   
 }

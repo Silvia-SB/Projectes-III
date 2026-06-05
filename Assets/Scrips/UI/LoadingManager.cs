4f7b9eb8a6268ee77464aa4a7340b5963ca0cbc5
Oriol Renom Plaja <oriol.renom.plaja@gmail.com>
2026-06-05 20:42:05 +0200

    Fix Fog

--- Assets/Scrips/UI/LoadingManager.cs
+++ Assets/Scrips/UI/LoadingManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.Rendering.Universal;
 public class LoadingManager : MonoBehaviour
 {
     [Header("Scene")]
@@ -16,6 +16,10 @@ public class LoadingManager : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private float visualSpeed = 0.7f;
     [SerializeField] private int minimumLoadingMilliseconds = 800;
+    
+    [Header("URP Full Screen Pass")]
+    [SerializeField] private ScriptableRendererData pcRendererData;
+    [SerializeField] private string fullScreenFeatureName = "FullScreenPassRendererFeature";
 
     private async void Start()
     {
@@ -76,8 +80,33 @@ public class LoadingManager : MonoBehaviour
         UpdateProgress(0.98f);
 
         await Task.Yield();
+        
+        SetRendererFeatureActive(fullScreenFeatureName, true);
 
         operation.allowSceneActivation = true;
+    }
+    
+    private void SetRendererFeatureActive(string targetFeatureName, bool active)
+    {
+        if (pcRendererData == null)
+        {
+            Debug.LogWarning("PC Renderer Data no asignado.");
+            return;
+        }
+
+        foreach (ScriptableRendererFeature feature in pcRendererData.rendererFeatures)
+        {
+            if (feature == null) continue;
+
+            if (feature.name == targetFeatureName)
+            {
+                feature.SetActive(active);
+                pcRendererData.SetDirty();
+                return;
+            }
+        }
+
+        Debug.LogWarning("No se encontró el Renderer Feature: " + targetFeatureName);
     }
 
     private void UpdateProgress(float progress)
